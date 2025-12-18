d743c7a95e59ad134aadca246771f2bdebaae626
reignem <mathy.reign@gmail.com>
2025-12-18 17:17:51 +0100

    Endpoint Session

---
+++ NexusAPI/Endpoints/Session/GetAllSessionEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using NexusAPI.DTO.Session.Response;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace NexusAPI.Endpoints.Session;
+
+public class GetAllSessionEndpoint(NexusDbContext db)
+    : EndpointWithoutRequest<List<GetSessionDto>>
+{
+    public override void Configure()
+    {
+        Get("/sessions");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var responseDto = await db.Sessions
+            .Select(a => new GetSessionDto
+            {
+                Id = a.Id,
+                DateTimeStart = a.DateTimeStart,
+                DateTimeEnd = a.DateTimeEnd,
+                Status = a.Status
+            })
+            .ToListAsync(ct);
+
+        await Send.OkAsync(responseDto, ct);
+    }
+}
