739a64fdcb14028bc9074cfd843804b3db25b463
colesm <colesmatheo@outlook.fr>
2025-12-18 16:28:43 +0100

    MAJ add dto's Activity

---
+++ NexusAPI/DTO/Activity/Request/CreateActivityDto.cs
@@ -0,0 +1,7 @@
+namespace NexusAPI.DTO.Activity.Request;
+
+public class CreateActivityDto
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
---
+++ NexusAPI/DTO/Activity/Response/GetActivityDto.cs
@@ -0,0 +1,8 @@
+namespace NexusAPI.DTO.Activity.Response;
+
+public class GetActivityDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
