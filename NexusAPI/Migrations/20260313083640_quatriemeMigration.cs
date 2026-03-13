61cda32f4df030764d4f96aa9da0314ded1bdf8f
colesm <colesmatheo@outlook.fr>
2026-03-13 09:41:34 +0100

    maj migration

---
+++ NexusAPI/Migrations/20260313083640_quatriemeMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NexusAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class quatriemeMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LoginId",
+                table: "Sessions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LoginId",
+                table: "Sessions");
+        }
+    }
+}
